4a883f163b9a447349e97a41e8bfcd9f7d1b4b1a
Richkwokkk <guofeinan@gmail.com>
2024-08-06 17:58:21 +0930

    Modified minishell code

--- minishell.c
+++ minishell.c
@@ -7,101 +7,123 @@
 #include <signal.h>
 #include <errno.h>
 
-#define NV 20        // max number of command tokens
-#define NL 100       // input buffer size
-char line[NL];       // command input buffer
+#define NV 20
+#define NL 100
 
-// Function to display the shell prompt
+char line[NL];
+
 void prompt(void) {
-    fprintf(stdout, "\n msh> ");
-    fflush(stdout);
+    if (fprintf(stdout, "\n msh> ") < 0) {
+        perror("fprintf");
+        exit(EXIT_FAILURE);
+    }
+    if (fflush(stdout) != 0) {
+        perror("fflush");
+        exit(EXIT_FAILURE);
+    }
+}
+
+// Signal handler for SIGCHLD
+void sigchld_handler(int signo) {
+    int status;
+    pid_t pid;
+    while ((pid = waitpid(-1, &status, WNOHANG)) > 0) {
+        printf("Background process %d finished\n", pid);
+    }
+    if (pid == -1 && errno != ECHILD) {
+        perror("waitpid");
+    }
 }
 
 int main(int argk, char *argv[], char *envp[]) {
-    int frkRtnVal;   // value returned by fork sys call
-    int wpid;        // value returned by wait
-    char *v[NV];     // array of pointers to command line tokens
-    char *sep = " \t\n"; // command line token separators
-    int i;           // parse index
-    int background;  // flag to check if command should be run in background
+    pid_t frkRtnVal;
+    int wpid;
+    char *v[NV];
+    char *sep = " \t\n";
+    int i;
+    int background = 0;
+
+    // Set up signal handler for SIGCHLD
+    struct sigaction sa;
+    sa.sa_handler = sigchld_handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART | SA_NOCLDSTOP;
+    if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+        perror("sigaction");
+        exit(EXIT_FAILURE);
+    }
 
     while (1) {
-        prompt(); 
-
-        // Read the command line input
+        prompt();
         if (fgets(line, NL, stdin) == NULL) {
-            // Check for end of file or error
             if (feof(stdin)) {
-                fprintf(stderr, "EOF detected, exiting...\n");
+                fprintf(stderr, "EOF pid %d feof %d ferror %d\n", getpid(), feof(stdin), ferror(stdin));
                 exit(0);
             } else {
-                perror("fgets error");
-                continue;
+                perror("fgets");
+                exit(EXIT_FAILURE);
             }
         }
 
-        // Ignore comments, empty lines, and null commands
+        if (ferror(stdin)) {
+            perror("ferror on stdin");
+            clearerr(stdin);
+        }
+
         if (line[0] == '#' || line[0] == '\n' || line[0] == '\000')
             continue;
 
-        // Tokenize the input line
         v[0] = strtok(line, sep);
-        for (i = 1; i < NV; i++) {
+        for (i = 1; i < NV - 1; i++) {  // Leave room for NULL at the end
             v[i] = strtok(NULL, sep);
             if (v[i] == NULL)
                 break;
         }
-        if (i == 0) continue; // No tokens found
-        v[i] = NULL; // Null-terminate the token array
+        v[i] = NULL;  // Ensure the argument list is NULL-terminated
 
-        background = 0; // Reset background flag
-        // Check if the last token is '&' to run command in background
-        if (i > 1 && strcmp(v[i - 1], "&") == 0) {
+        // Check for background process
+        background = 0;
+        if (i > 0 && strcmp(v[i-1], "&") == 0) {
             background = 1;
-            v[i - 1] = NULL; // Remove '&' from the token array
+            v[i-1] = NULL;  // Remove the '&' from the argument list
         }
 
-        // Handle 'cd' command separately
-        if (strcmp(v[0], "cd") == 0) {
+        // Handle built-in 'cd' command
+        if (v[0] != NULL && strcmp(v[0], "cd") == 0) {
             if (v[1] == NULL) {
-                fprintf(stderr, "cd: missing argument\n");
+                // Change to home directory if no argument is provided
+                if (chdir(getenv("HOME")) != 0) {
+                    perror("chdir to HOME");
+                }
             } else {
                 if (chdir(v[1]) != 0) {
-                    perror("cd error");
+                    perror("chdir");
                 }
             }
             continue;
         }
 
-        // Fork a child process to execute the command
         switch (frkRtnVal = fork()) {
-        case -1: // Error in fork
-            perror("fork error");
+        case -1:
+            perror("fork");
             break;
-
-        case 0: // Child process
-            execvp(v[0], v); // Execute the command
-            perror("execvp error"); // Exec failed
-            exit(1); // Exit child process with error
-
+        case 0:
+            // Child process
+            execvp(v[0], v);
+            // If execvp returns, it must have failed
+            perror("execvp");
+            exit(EXIT_FAILURE);  // Terminate child process if exec fails
         default:
+            // Parent process
             if (!background) {
-                // Wait for child process to complete if not running in background
-                if (waitpid(frkRtnVal, NULL, 0) < 0) {
-                    perror("waitpid error");
+                wpid = wait(NULL);
+                if (wpid == -1) {
+                    perror("wait");
+                } else {
+                    printf("%s done \n", v[0]);
                 }
-                printf("%s done\n", v[0]);
             } else {
-                // Report that command is running in background
-                printf("%s running in background\n", v[0]);
-            }
-
-            // Check for background processes that have finished
-            while ((wpid = waitpid(-1, NULL, WNOHANG)) > 0) {
-                printf("Background process %d done\n", wpid);
-            }
-            if (wpid < 0 && errno != ECHILD) {
-                perror("waitpid error");
+                printf("Started background process %d\n", frkRtnVal);
             }
             break;
         }
