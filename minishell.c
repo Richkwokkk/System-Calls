f4b112980911b1248451b0b744038a81152b8041
Richkwokkk <guofeinan@gmail.com>
2024-08-10 23:20:00 +0930

    Modified code logic based on details provided by tutor

--- minishell.c
+++ minishell.c
@@ -7,43 +7,52 @@
 #include <signal.h>
 #include <errno.h>
 
-#define NV 20        // max number of command tokens
-#define NL 100       // input buffer size
-char line[NL];       // command input buffer
+#define NV 20
+#define NL 100
+char line[NL];
+int bg_count = 0; // Counter for background processes
 
-// Function to display the shell prompt
-void prompt(void) {
-    fprintf(stdout, "\n msh> ");
-    fflush(stdout);
-}
+typedef struct BackgroundProcess {
+    int id;
+    pid_t pid;
+    char command[NL];
+} BackgroundProcess;
+
+BackgroundProcess bg_processes[NV];
+int bg_index = 0;
 
 void handle_background_processes() {
-    int wpid;        // value returned by wait
-    while ((wpid = waitpid(-1, NULL, WNOHANG)) > 0) {
-        printf("Background process %d done\n", wpid);
-    }
-    if (wpid < 0 && errno != ECHILD) {
-        perror("waitpid error");
+    int wpid;
+    int status;
+    while ((wpid = waitpid(-1, &status, WNOHANG)) > 0) {
+        for (int i = 0; i < bg_index; i++) {
+            if (bg_processes[i].pid == wpid) {
+                fprintf(stdout, "[%d]+ Done\t%s\n", bg_processes[i].id, bg_processes[i].command);
+                fflush(stdout);
+                bg_processes[i] = bg_processes[--bg_index]; // Remove the finished process from the array
+                break;
+            }
+        }
+
+        // Reset bg_count if no more background processes are running
+        if (bg_index == 0) {
+            bg_count = 0; // Reset the counter when all background processes are done
+        }
     }
 }
 
 int main(int argk, char *argv[], char *envp[]) {
-    int frkRtnVal;   // value returned by fork sys call
-    char *v[NV];     // array of pointers to command line tokens
-    char *sep = " \t\n"; // command line token separators
-    int i;           // parse index
-    int background;  // flag to check if command should be run in background
-
-    signal(SIGCHLD, handle_background_processes); // Handle background process completion
+    int frkRtnVal;
+    char *v[NV];
+    char *sep = " \t\n";
+    int i;
+    int background;
 
     while (1) {
-        prompt(); 
-
-        // Read the command line input
+        // fprintf(stdout, "\n msh> "); // Remove prompt line
         if (fgets(line, NL, stdin) == NULL) {
-            // Check for end of file or error
             if (feof(stdin)) {
-                fprintf(stderr, "EOF detected, exiting...\n");
+                // Exit without additional output on EOF
                 exit(0);
             } else {
                 perror("fgets error");
@@ -51,28 +60,25 @@ int main(int argk, char *argv[], char *envp[]) {
             }
         }
 
-        // Ignore comments, empty lines, and null commands
         if (line[0] == '#' || line[0] == '\n' || line[0] == '\000')
             continue;
 
-        // Tokenize the input line
         v[0] = strtok(line, sep);
         for (i = 1; i < NV; i++) {
             v[i] = strtok(NULL, sep);
             if (v[i] == NULL)
                 break;
         }
-        if (i == 0) continue; // No tokens found
-        v[i] = NULL; // Null-terminate the token array
+        if (i == 0) continue;
+        v[i] = NULL;
 
-        background = 0; // Reset background flag
-        // Check if the last token is '&' to run command in background
+        background = 0;
         if (i > 1 && strcmp(v[i - 1], "&") == 0) {
             background = 1;
-            v[i - 1] = NULL; // Remove '&' from the token array
+            v[i - 1] = NULL;
+            bg_count++; // Increment background process counter
         }
 
-        // Handle 'cd' command separately
         if (strcmp(v[0], "cd") == 0) {
             if (v[1] == NULL) {
                 fprintf(stderr, "cd: missing argument\n");
@@ -86,28 +92,34 @@ int main(int argk, char *argv[], char *envp[]) {
             continue;
         }
 
-        // Fork a child process to execute the command
         switch (frkRtnVal = fork()) {
-        case -1: // Error in fork
+        case -1:
             perror("fork error");
             break;
 
-        case 0: // Child process
-            execvp(v[0], v); // Execute the command
-            perror("execvp error"); // Exec failed
-            exit(1); // Exit child process with error
+        case 0:
+            execvp(v[0], v);
+            perror("execvp error");
+            exit(1);
 
         default:
-            if (!background) {
-                // Wait for child process to complete if not running in background
+            if (background) {
+                fprintf(stdout, "[%d] %d\n", bg_count, frkRtnVal);
+                fflush(stdout);
+                // Store background process details for later use
+                bg_processes[bg_index].id = bg_count;
+                bg_processes[bg_index].pid = frkRtnVal;
+                snprintf(bg_processes[bg_index].command, NL, "%s %s", v[0], v[1] ? v[1] : "");
+                bg_index++;
+            } else {
                 if (waitpid(frkRtnVal, NULL, 0) < 0) {
                     perror("waitpid error");
                 }
-                printf("%s done\n", v[0]);
-            } else {
-                // Report that command is running in background
-                printf("%s running in background\n", v[0]);
             }
+
+            // Check for completed background processes
+            handle_background_processes();
+
             break;
         }
     }
